d5caccf377dbafd4caec292891c4c1eeb5fbe5f8
Tristan Neumann <tristan.neumann@hotmail.com>
2018-10-11 19:08:24 -0700

    Fix image seeding by streaming; Fully download Champs and Summs;

--- seedStatic/seedChampPics.js
+++ seedStatic/seedChampPics.js
@@ -1,23 +1,27 @@
-const Champs = require('../db/models/Champions.js');
 const fs = require('fs');
 const axios = require('axios');
-const picsErred = [];
+const Champs = require('../db/models/Champions');
 
 Champs.find({}, 'image', (err, champsData) => {
   if (err) { console.error(err); } else {
+    console.log('champData: ', champsData[0]);
     champsData.forEach((champData) => {
       console.log('image?: ', champData.image.full);
-      axios.get(`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/${champData.image.full}`)
+      const file = fs.createWriteStream(`./images/champions/${champData.image.full}`);
+      axios.get(`http://ddragon.leagueoflegends.com/cdn/8.18.1/img/champion/${champData.image.full}`, { responseType: 'stream' })
         .then((pic) => {
-          // console.log(pic);
-          fs.writeFile(`./images/champions/${champData.image.full}`, pic.data, (err) => {
-            if (err) {
-              console.error(err);
-              picsErred.push(champData.image.full);
-            }
-            console.log(picsErred, ' pic not Downloaded\n');
+          pic.data.pipe(file);
+
+          file.on('finish', () => {
+            file.close(() => {
+              console.log(champData.image.full, ' downloaded');
+            }); // close() is async, call cb after close completes.
+          }).on('error', (err) => { // Handle errors
+            fs.unlink(dest); // Delete the file async. (But we don't check the result)
+            console.error(err);
           });
         })
+        .catch(err => console.error(err));
     });
   }
 });
--- seedStatic/seedSummPics.js
+++ seedStatic/seedSummPics.js
@@ -1,23 +1,27 @@
-const Summs = require('../db/models/Summoners');
 const fs = require('fs');
 const axios = require('axios');
-const picsErred = [];
+const Summs = require('../db/models/Summoners');
 
 Summs.find({}, 'image', (err, champsData) => {
   if (err) { console.error(err); } else {
+    console.log('champData: ', champsData[0]);
     champsData.forEach((summData) => {
-      console.log('image?: ', summData.image);
-      axios.get(`http://ddragon.leagueoflegends.com/cdn/6.24.1/img/spell/${summData.image.full}`)
+      console.log('image?: ', summData.image.full);
+      const file = fs.createWriteStream(`./images/summoners/${summData.image.full}`);
+      axios.get(`http://ddragon.leagueoflegends.com/cdn/8.18.1/img/spell/${summData.image.full}`, {responseType: 'stream'})
         .then((pic) => {
-          // console.log(pic);
-          fs.writeFile(`./images/summoners/${summData.image.full}`, pic.data, (err) => {
-            if (err) {
-              console.error(err);
-              picsErred.push(summData.image.full);
-            }
-            console.log(picsErred, ' pic not Downloaded\n');
+          pic.data.pipe(file);
+
+          file.on('finish', () => {
+            file.close(() => {
+              console.log(summData.image.full, ' downloaded');
+            }); // close() is async, call cb after close completes.
+          }).on('error', (err) => { // Handle errors
+            fs.unlink(dest); // Delete the file async. (But we don't check the result)
+            console.error(err);
           });
-        });
+        })
+        .catch(err => console.error(err));
     });
   }
 });
