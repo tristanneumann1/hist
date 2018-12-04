71abd8f76c0ca48eadf07819e482a4712ee46501
Tristan Neumann <tristan.neumann@hotmail.com>
2018-12-04 01:30:19 -0800

    set port up for heroku;

--- server/index.js
+++ server/index.js
@@ -2,5 +2,5 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').load();
 }
 const app = require('./express.js');
-const port = process.env.port || 3000;
+const port = process.env.PORT || process.env.port || 3000;
 app.listen(port, ()=>console.log('Server listening on port: ', port));
